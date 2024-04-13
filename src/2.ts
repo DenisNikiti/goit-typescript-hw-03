54019a5f33baf8146152f73d31ff90694ffe6d35
DenisNikiti <den.hikitin@gmail.com>
2024-04-13 22:58:57 +0300

    5

--- src/2.ts
+++ src/2.ts
@@ -17,10 +17,12 @@
 class Employee {
   // Заповніть модифікатори доступу
   name: string;
-  department: string;
-  salary: number;
 
-  constructor(name: string, department: string, salary: number) {
+  constructor(
+    name: string,
+    private department: string,
+    protected salary: number
+  ) {
     this.name = name;
     this.department = department;
     this.salary = salary;
@@ -33,7 +35,9 @@ class Employee {
 
 class Manager extends Employee {
   // Реалізуйте конструктор та збільшіть salary на 10000
+  constructor(name: string, department: string, salary: number) {
+    super(name, department, salary + 10000);
+  }
 }
-
 
 export {};
